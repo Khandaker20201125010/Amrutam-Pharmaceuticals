4598268f4353e1cffa81b266e52d383de6946e9d
unknown <prantokih42@gmail.com>
2024-12-22 17:59:11 +0600

    new route created

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router'
 import {
   createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
-import Main from './Layout/Main';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main></Main>,
-    children: [
-      {
-        path: '/',
-        
-      },
-    ]
-  }
-
+    element: <div>Hello world!</div>,
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router} />
+     <RouterProvider router={router} />
   </StrictMode>,
 )

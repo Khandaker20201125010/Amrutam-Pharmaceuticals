0236a375b08444a93007cbf615aa5fefbd0e886d
unknown <prantokih42@gmail.com>
2024-12-22 18:13:56 +0600

    banner added again

---
+++ src/routes/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <div>
+        {/* <Banner></Banner>
+        <AyurvedasMagic></AyurvedasMagic>
+        <Conculations></Conculations>
+        <ConsulationBanner></ConsulationBanner>
+        <AyurvedasAppoarch></AyurvedasAppoarch>
+        <CustomerReview></CustomerReview>
+        <Experts></Experts>
+        <HomeApp></HomeApp>
+   */}
+      </div>
+    );
+};
+
+export default Home;

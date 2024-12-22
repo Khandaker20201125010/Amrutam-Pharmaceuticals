cdb6f39f11b2cbfca08c162df55f6f5f3d5eef69
unknown <prantokih42@gmail.com>
2024-12-22 16:38:38 +0600

    main laybout created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router'
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Main from './Layout/Main';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        
+      },
+    ]
+  }
+
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
